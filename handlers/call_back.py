4cbf199c76669bfa4ee18c876881abf8a3ebf1dc
Islam <it.kaktus312@gmail.com>
2024-02-06 00:38:29 +0600

    Basic setup and connection of the bot

---
+++ handlers/call_back.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+from aiogram import types, Dispatcher
+from config import bot
+from database.sql_commands import DataBase
+from keyboards.inline_buttons import questionnaire_keyboard
+
+
+async def start_questionnaire_call(call: types.CallbackQuery):
+    await bot.send_message(
+        chat_id=call.from_user.id,
+        text='Python or Mojo ?',
+        reply_markup=await questionnaire_keyboard()
+    )
+
+
+async def python_call(call: types.CallbackQuery):
+    await bot.send_message(
+        chat_id=call.from_user.id,
+        text='You are Python Developer 🚀',
+    )
+
+
+async def mojo_call(call: types.CallbackQuery):
+    await bot.send_message(
+        chat_id=call.from_user.id,
+        text='You are Mojo Developer 🔥',
+    )
+
+
+def register_call_back_handlers(dp: Dispatcher):
+    dp.register_callback_query_handler(start_questionnaire_call,
+                                       lambda call: call.data == 'start_questionnaire')
+    dp.register_callback_query_handler(python_call,
+                                       lambda call: call.data == 'python')
+    dp.register_callback_query_handler(mojo_call,
+                                       lambda call: call.data == 'mojo')
